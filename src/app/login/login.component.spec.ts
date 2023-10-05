857a8d3edef59f0c8bcdc648b1eed6159028cc02
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-10-05 13:30:57 +0530

    Update login.component.spec.ts

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -8,9 +8,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +19,9 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('boundary', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
